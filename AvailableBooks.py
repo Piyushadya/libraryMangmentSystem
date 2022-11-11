c2f3c7936f8164cee5ba68265b7ed42500c63a6e
Piyushadya <108247171+Piyushadya@users.noreply.github.com>
2022-11-11 12:49:39 -0500

    initial commit

--- AvailableBooks.py
+++ AvailableBooks.py
@@ -32,14 +32,14 @@ def AvailableBooks():
     labelFrame.place(relx=0,rely=0.3,relwidth=1,relheight=0.5)
     y = 0.25
     
-    Label(labelFrame, text="%-10s%-20s%-20s%-20s"%('BID','Title','Author','Status'),bg='black',fg='white',font=('Courier', 20)).place(relx=0.01,rely=0.1)
+    Label(labelFrame, text="%-10s%-20s%-20s"%('BID','Title','Author'),bg='black',fg='white',font=('Courier', 20)).place(relx=0.01,rely=0.1)
     Label(labelFrame, text="--------------------------------------------------------------------------------------------------------------------------------",bg='black',fg='white',font=('Courier',20)).place(relx=0,rely=0.2)
     getBooks = "SELECT * FROM books where status ='avail'"; # change books with your table name
     try:
         cur.execute(getBooks)
         con.commit()
         for i in cur:
-            Label(labelFrame, text="%-10s%-20s%-20s%-20s"%(i[0],i[1],i[2],i[3]),bg='black',fg='white',font=('Courier', 20)).place(relx=0.01,rely=y)
+            Label(labelFrame, text="%-10s%-20s%-20s"%(i[0],i[1],i[2]),bg='black',fg='white',font=('Courier', 20)).place(relx=0.01,rely=y)
             y += 0.1
     except:
         messagebox.showinfo("Failed to fetch files from database")
