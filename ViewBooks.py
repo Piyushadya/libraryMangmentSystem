6d5d648ea02c2492c3cd71dd169e03eec1cb0ef4
Piyushadya <108247171+Piyushadya@users.noreply.github.com>
2022-12-01 16:02:43 -0500

    Update ViewBooks.py
    Add comments

--- ViewBooks.py
+++ ViewBooks.py
@@ -4,18 +4,20 @@ from tkinter import messagebox
 from DatabaseConnectivity import *
 
 
-
+# It display all books to the user
 def View():
     global bookTable
 
-    # Add your own database name and password here to reflect in the code
+    # It will provide database connectivity
     con=DatabaseConnectivity()
     cur = con.cursor()
 
-    # Enter Table Names here
+    # Table name is mentioned here
     bookTable = "books"
 
     root = Tk()
+    
+    # Form sizing
     root.title("Library")
     root.minsize(width=1100, height=700)
     root.geometry("600x500")
@@ -27,6 +29,7 @@ def View():
     headingFrame1 = Frame(root)
     headingFrame1.place(relx=0.25, rely=0.1, relwidth=0.5, relheight=0.13)
 
+    # Heading of the form 
     headingLabel = Label(headingFrame1, text="View Books", bg='black', fg='white', font=('Courier', 20))
     headingLabel.place(relx=0, rely=0, relwidth=1, relheight=1)
 
@@ -34,6 +37,7 @@ def View():
     labelFrame.place(relx=0, rely=0.3, relwidth=1, relheight=0.5)
     y = 0.25
 
+    # It will display all fields as column in particular page and show the fetched values accordingly in the respective fields
     Label(labelFrame,
           text="%-10s%-20s%-20s%-20s%-20s%-20s" % ('BID', 'Title', 'Author', 'Status', 'StudentNumber', 'StudentName'),
           bg='black', fg='white',font=('Courier', 16)).place(relx=0.01, rely=0.1)
@@ -52,6 +56,7 @@ def View():
     except:
         messagebox.showinfo("Failed to fetch files from database")
 
+    # Quit button to destroy the current event 
     quitBtn = Button(root, text="Quit", bg='#f7f1e3', fg='black', command=root.destroy,font=('Courier', 15))
     quitBtn.place(relx=0.4, rely=0.9, relwidth=0.18, relheight=0.08)
 
