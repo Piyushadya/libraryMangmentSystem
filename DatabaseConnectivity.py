f8b4a035b738935e0cb8c04305aa51af4b1c3742
Piyushadya <108247171+Piyushadya@users.noreply.github.com>
2022-12-01 11:36:36 -0500

    Update DatabaseConnectivity.py
    Add comments

--- DatabaseConnectivity.py
+++ DatabaseConnectivity.py
@@ -1,10 +1,13 @@
 import pymysql
 
+# It will provide connection to the database
 def DatabaseConnectivity():
 
+    # Provide Database credentials
     mypass = "root1234"
     mydatabase = "db"
 
+    # Connection to the database
     con = pymysql.connect(host="localhost", user="root", password=mypass, database=mydatabase)
     return con
 
