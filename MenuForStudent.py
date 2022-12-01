b5a878d5ac9dd3098d690770c932678623899a3a
Piyushadya <108247171+Piyushadya@users.noreply.github.com>
2022-12-01 14:29:07 -0500

    Update MenuForStudent.py
    Add comments

--- MenuForStudent.py
+++ MenuForStudent.py
@@ -3,21 +3,19 @@ from tkinter import *
 from PIL import ImageTk, Image
 import pymysql
 from tkinter import messagebox
-from AddBook import *
-from DeleteBook import *
 from ViewBooks import *
-from IssueBook import *
-from ReturnBook import *
 from ViewIssuedbooks import *
 from DatabaseConnectivity import *
+
+# It will display Menu form to the student after successful login
 def menuForStudent(memId):
     global cur
 
-    # Add your own database name and password here to reflect in the code
-    # Add your own database name and password here to reflect in the code
+    # It will provide database connectivity
     con=DatabaseConnectivity()
     cur = con.cursor()
 
+    # Form sizing
     root = Tk()
     root.title("Library")
     root.minsize(width=1100, height=700)
@@ -25,19 +23,23 @@ def menuForStudent(memId):
 
     Canvas1 = Canvas(root)
 
+    #Background color
     Canvas1.config(bg="#ADD8E6")
     Canvas1.pack(expand=True, fill=BOTH)
 
     headingFrame1 = Frame(root)
     headingFrame1.place(relx=0.2, rely=0.1, relwidth=0.6, relheight=0.16)
 
+    #Heading of student menu
     headingLabel = Label(headingFrame1, text="Menu For Student", bg='black', fg='white',
                          font=('Courier', 20))
     headingLabel.place(relx=0, rely=0, relwidth=1, relheight=1)
 
+    # If clicked, it will display the available books
     btn1 = Button(root, text="Available Books ", bg='black', fg='white',command=AvailableBooks,font=('Courier', 15))
     btn1.place(relx=0.28, rely=0.4, relwidth=0.45, relheight=0.1)
 
+    # If clicked, it will display the books issued to particular user
     btn2 = Button(root, text="View Issued Books ", bg='black', fg='white',command=lambda: ViewIssued(memId),font=('Courier', 15))
     btn2.place(relx=0.28, rely=0.5, relwidth=0.45, relheight=0.1)
 
