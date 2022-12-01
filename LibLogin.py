4790a8de3639934ef2d91f681502e0281742f42e
Piyushadya <108247171+Piyushadya@users.noreply.github.com>
2022-12-01 14:19:50 -0500

    Update LibLogin.py
    Add comments

--- LibLogin.py
+++ LibLogin.py
@@ -5,6 +5,7 @@ import pymysql
 from MenuForLibrarian import *
 from DatabaseConnectivity import *
 
+# This function is added to fetch the data from the user form and check if the credentials are Valid or not
 def librarianLogin():
     memId = loginInfo1.get()
     username = loginInfo2.get()
@@ -25,28 +26,28 @@ def returnback():
 
     root.destroy()
 
-
+# This function is added to display the form to the user, where they can add login credentials
 def loginLibrarian():
     global loginInfo1,loginInfo2,loginInfo3,Canvas1,con,cur,studentTable,root
     root = Tk()
     root.title("Library")
     root.minsize(width=1100, height=700)
     root.geometry("600x500")
-    # Add your own database name and password here to reflect in the code
+    
+    # It will provide database connectivity
     con=DatabaseConnectivity()
     cur = con.cursor()
 
-
-    studentTable = "members" 
-
     Canvas1 = Canvas(root)
 
+    #Background color
     Canvas1.config(bg="#ff6e40")
     Canvas1.pack(expand=True,fill=BOTH)
         
     headingFrame1 = Frame(root)
     headingFrame1.place(relx=0.25,rely=0.1,relwidth=0.5,relheight=0.13)
 
+    # Heading of the form
     headingLabel = Label(headingFrame1, text="Login for Librarian", bg='black', fg='white', font=('Courier',20))
     headingLabel.place(relx=0,rely=0, relwidth=1, relheight=1)
 
@@ -54,13 +55,14 @@ def loginLibrarian():
     labelFrame = Frame(root,bg='black')
     labelFrame.place(relx=0.1,rely=0.4,relwidth=0.8,relheight=0.4)
         
-    # student ID
+    # Login ID for librarian
     lb1 = Label(labelFrame,text="Member ID : ", bg='black', fg='white',font=('Courier', 15))
     lb1.place(relx=0.05,rely=0.05, relheight=0.08)
   
     loginInfo1 = Entry(labelFrame)
     loginInfo1.place(relx=0.3,rely=0.05, relwidth=0.62, relheight=0.08)
 
+    # Username of librarian
     lb2 = Label(labelFrame,text="Username : ", bg='black', fg='white',font=('Courier', 15))
     lb2.place(relx=0.05,rely=0.2, relheight=0.08)
 
@@ -69,15 +71,17 @@ def loginLibrarian():
 
     lb3 = Label(labelFrame,text="Password : ", bg='black', fg='white',font=('Courier', 15))
     lb3.place(relx=0.05,rely=0.35, relheight=0.08)
-        
+     
+    # Encryption for the password
     loginInfo3 = Entry(labelFrame, show = "*")
     loginInfo3.place(relx=0.3,rely=0.35, relwidth=0.62, relheight=0.08)
     
             
         
-    #Submit Button
+    # submit button will trigger LibrarianLogin function, where credentials are checked
     SubmitBtn = Button(root,text="SUBMIT",bg='#d1ccc0', fg='black',command=librarianLogin,font=('Courier', 15))
     SubmitBtn.place(relx=0.23,rely=0.9, relwidth=0.18,relheight=0.08)
     
+    # Return button is added and will return user to the main window of the application
     quitBtn = Button(root,text="RETURN",bg='#f7f1e3', fg='black', command=returnback,font=('Courier', 15))
     quitBtn.place(relx=0.46,rely=0.9, relwidth=0.18,relheight=0.08)
